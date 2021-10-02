4a37fd0de3f61b806d09a3fd0e525dbe64007c77
Nicolas Barbosa <nicolas.barbosa@orange.com>
2021-10-02 20:20:57 +0200

    Initial Commit

---
+++ Churlets Point/Churlets_PointApp.swift
@@ -0,0 +1,17 @@
+//
+//  Churlets_PointApp.swift
+//  Churlets Point
+//
+//  Created by Nicolas Barbosa on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Churlets_PointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
